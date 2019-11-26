51524e38e0dd9492376c50e37d2c370855e85267
枳本望 <nozomusp29@gmail.com>
2019-11-26 10:31:05 +0800

    indexファイル　layoutファイル　MovieController作成

---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,14 @@
+<!-- layout.blade.phpを読み込む -->
+@extends('layout')
+
+@section('title', 'ホーム')
+
+@section('content')
+<header>
+<div class="container">
+
+</div>
+</header>
+ 
+
+  @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','MovieController@index')->name('movie.index');
