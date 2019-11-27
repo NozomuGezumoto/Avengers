2c870683582570a5b8d17dfa8972732081894dd7
枳本望 <nozomusp29@gmail.com>
2019-11-27 11:22:53 +0800

    index.blade.php image横並び

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -5,16 +5,20 @@
 
 @section('content')
 
-<div class="container">
+<div class="container_serch">
   <form action="{{ route('movie.search') }}" method="get">
       <input type="text" class="form-control" name="movie_title" id="movie_title" placeholder="映画のタイトル">
       <button type="submit" class="btn btn-primary">Submit</button>
   </form>
+  <div class="container_img">
+    <div class="row">
   @foreach ($new_movies as $new_movie)
-      <p>{{$new_movie->title}}</p>
-      {{-- 300,450 --}}
-      <img src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/{{$new_movie->poster_path}}">
+        {{-- <p class="movie_title">{{$new_movie->title}}</p> --}}
+        {{-- 300,450 --}}
+        <img src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/{{$new_movie->poster_path}}" class="rounded-sm">
       {{-- <p>{{$new_movie->overview}}</p> --}}
-  @endforeach
+      @endforeach
+    </div>
+  </div>
 
   @endsection
