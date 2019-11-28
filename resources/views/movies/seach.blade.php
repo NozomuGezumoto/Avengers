164d89b47ebfd842394fd396d2c8e7196d186ae1
枳本望 <nozomusp29@gmail.com>
2019-11-28 11:39:30 +0800

    index seach review css改善

--- resources/views/movies/seach.blade.php
+++ resources/views/movies/seach.blade.php
@@ -5,13 +5,17 @@
 @section('content')
 {{-- <div>{{ $movies }}</div> --}}
     @foreach ($movies as $movie)
-    <form action="{{ route('movie.review', ['id' => $movie->id]) }}" method="get">
-        <a href="{{ route('movie.review', ['id' => $movie->id]) }}">
+     <div class="container_list">
+      <div class="row">
+        <form action="{{ route('movie.review', ['id' => $movie->id]) }}" method="get">
+          <a href="{{ route('movie.review', ['id' => $movie->id]) }}">
           <p>{{$movie->title}}</p>
           <img src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/{{$movie->poster_path}}"></button>
-          <p>{{$movie->overview}}</p>
-        </a>
+          {{-- <p>{{$movie->overview}}</p> --}}
+          </a>
         {{-- <p>{{$movie->}}}</p> --}}
-
+        </form>
+      </div>
+    </div>
     @endforeach
 @endsection
