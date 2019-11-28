da95f66293c4019e9a70e412ee0387c45d7a7d11
枳本望 <nozomusp29@gmail.com>
2019-11-28 22:04:20 +0800

    微調整

--- resources/views/movies/review.blade.php
+++ resources/views/movies/review.blade.php
@@ -7,14 +7,14 @@
   <div class="container_list">
     <div class="row">
       <form action="{{ route('movie.exchange') }}" method="get">
-        <p>{{$id->title}}</p>
+        {{-- <p>{{$id->title}}</p> --}}
         <img src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/{{$id->poster_path}}">
         {{-- <p>{{$id->overview}}</p> --}}
+        <div class="btn_review">
+          <button type="submit" class="btn-outline-warning btn-lg">review</button>
+        </div>
       </form>
     </div>
-  </div>
-  <div class="btn_review">
-    <button type="submit" class="btn-outline-warning btn-lg">review</button>
   </div>
 
   @endsection
