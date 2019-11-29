8dceec16dcd72fd9245d785616ac8080a30e4062
Nagasawa Yoshitoki <>
2019-11-29 09:04:04 +0800

    登録画面等のページを作成したが、他のメンバーもコードを書いていたため一時保留

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,9 @@ Route::get('/', 'MovieController@index')->name('movie.index');
 Route::get('/search', 'MovieController@search')->name('movie.search');
 Route::get('/review/{id}', 'MovieController@review')->name('movie.review');
 Route::get('/exchange', 'MovieController@exchange')->name('movie.exchange');
+
+Route::get('/register', 'MovieController@register')->name('movie.register');
+Route::get('/email', 'MovieController@email')->name('movie.email');
+Route::get('/login', 'MovieController@login')->name('movie.login');
+Route::get('/reset', 'MovieController@reset')->name('movie.reset');
+Route::get('/verify', 'MovieController@verify')->name('movie.verify');
