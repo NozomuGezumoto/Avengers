53dc02056c5e12ab4fbbe4cbfde9df40990dd550
Nagasawa Yoshitoki <>
2019-11-29 11:43:15 +0800

    メモ追加

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,13 @@ Auth::routes();
 Route::group(['middleware' => ['auth']], function ()
 {
 Route::get('/', 'MovieController@index')->name('movie.index');
-Route::get('/search', 'MovieController@search')->name('movie.search');
+
+//ログイン
+Route::get('/search1', 'MovieController@search')->name('movie.search');
+
+Route::get('/search', 'MovieController@searchicon')->name('movie.searchicon');
 Route::get('/review/{id}', 'MovieController@review')->name('movie.review');
 Route::get('/exchange', 'MovieController@exchange')->name('movie.exchange');
-
-});
+Route::get('/Mypage', 'MovieController@Mypage')->name('movie.Mypage');
 Route::get('/register', 'MovieController@register')->name('movie.register');
-Route::get('/email', 'MovieController@email')->name('movie.email');
-Route::get('/login', 'MovieController@login')->name('movie.login');
-Route::get('/reset', 'MovieController@reset')->name('movie.reset');
-Route::get('/verify', 'MovieController@verify')->name('movie.verify');
+});
