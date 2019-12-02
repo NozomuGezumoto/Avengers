2c67f0034ec18b7ee06f77609d1aba62066e01c0
Nagasawa Yoshitoki <>
2019-12-02 10:47:29 +0800

    ２点修正と変更 ・データベースに画像関係のテーブルを追加 ・新規登録画面を追加

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,8 @@ Route::get('/', 'MovieController@index')->name('movie.index');
 
 //ログイン
 Route::get('/search1', 'MovieController@search')->name('movie.search');
-
 Route::get('/search', 'MovieController@searchicon')->name('movie.searchicon');
 Route::get('/review/{id}', 'MovieController@review')->name('movie.review');
 Route::get('/exchange', 'MovieController@exchange')->name('movie.exchange');
 Route::get('/Mypage', 'MovieController@Mypage')->name('movie.Mypage');
-Route::get('/register', 'MovieController@register')->name('movie.register');
 });
