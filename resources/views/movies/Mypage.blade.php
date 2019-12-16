0ad2b3c8d0a70115ef955efea1778e2d62e16b36
Nagasawa Yoshitoki <>
2019-12-16 17:50:46 +0800

    マイページのレイアウトを修正

--- resources/views/movies/Mypage.blade.php
+++ resources/views/movies/Mypage.blade.php
@@ -3,25 +3,31 @@
 @section('title', 'マイページ')
 
 @section('content')
+
+<header>
+</header>
+
+<br>
+
 <body>
 {{-- へッターの中に一時的にログイン・ログアウトできる機能をつける app.blade.php--}}
-    <header>
-      <div class="row">
+
+    {{-- <header>
+        <div class="row">
             <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
+
             <a class="navbar-brand" href="{{ url('/') }}">
                 {{ config('app.name', 'Laravel') }}
             </a>
+
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-
-                </ul>
-
+                <ul class="navbar-nav mr-auto"></ul>
                 <!-- Right Side Of Navbar -->
                 <ul class="navbar-nav ml-auto">
                     <!-- Authentication Links -->
@@ -35,21 +41,23 @@
                             </li>
                         @endif
                     @else
+
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
+                                {{ Auth::user()->name }} 
+                                <span class="caret"></span>
                             </a>
-
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
+                                    document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
-
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                     @csrf
                                 </form>
+
                               </div>
                           </li>
                      @endguest
@@ -58,46 +66,64 @@
           </div>
       </nav>
     </div>
-  </header>
+  </header> --}}
 
-  <h3>プロフィール</h3>
-
-
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('プロフィール') }}
+                    </div>
+                    <div class="card-body">
 
 {{-- イメージ画像表示 --}}
-<a href="{!! action('MovieController@ChangeImage') !!}">
-    <div class="nav-item" style="text-align:center" button>
-        <img height="80px" src="{{ asset(Auth::user()->picture_path) }}" >
+                        <a href="{!! action('MovieController@ChangeImage') !!}">
+                            <div class="nav-item" style="text-align:center" button>
+                                <img height="80px" src="{{ asset(Auth::user()->picture_path) }}" >
+                            </div>
+                        </a>
+                        <div style="margin-top: 30px;">
+                            <table class="table table-striped">
+                                <tr>
+                                    <th>{{ __('氏名') }}</th>
+                                    <td>{{ Auth::user()->name }}</td>
+                                </tr>
+                                <tr>
+                                    <th>{{ __('アドレス') }}</th>
+                                    <td>{{ Auth::user()->email }}</td>
+                                </tr>
+                                <tr>
+                                    <th>{{ __('コメント') }}</th>
+                                    <td>{{ Auth::user()->comment }}</td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                    <div class="nav-item dropdown"></div>
+                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        ログアウト
+                                        {{-- {{ Auth::user()->name }}  --}}
+                                        <span class="caret"></span>
+                                        </a>
+                                    <div class="dropdown-menu dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                    </th>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</a>
 
-<div style="margin-top: 30px;">
-   
-<table class="table table-striped">  
-<tr>
-<th>氏名</th>
-<td>{{ Auth::user()->name }}</td>
-</tr>  
-<tr>
-<th>メールアドレス</th>
-<td>{{ Auth::user()->email }}</td>
-</tr>  
-<tr>
-<th>コメント</th>
-<td>{{ Auth::user()->comment }}</td>
-</tr>
-</table>
- 
-</div>
-{{-- へッターの中に一時的にログイン・ログアウトできる機能をつける app.blade.php--}}
- @yield('content')
- <footer>
+    @yield('content')
 
-   {{-- <div class="footer">
-      <i class="fas fa-home icons"></i>
-      <i class="fas fa-search icons"></i>
-      <i class="far fa-heart icons"></i>
-      <i class="far fa-user icons"></i>
-   </div> --}}
- </footer>
- @endsection
+@endsection
