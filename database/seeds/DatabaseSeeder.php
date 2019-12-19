ff6c2904ab7cec91559fcb90711fd91357774ea4
枳本望 <nozomusp29@gmail.com>
2019-12-19 10:52:23 +0800

    no message

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
 
         $this->call(ImgsTableSeeder::class);
 
-        $this->call(ReviewsTableSeeder::class);
+        // $this->call(ReviewsTableSeeder::class);
     }
 }
