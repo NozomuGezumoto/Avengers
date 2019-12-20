14fb6ddcdb323a6a226fa2212ab049f7701c7196
枳本望 <nozomusp29@gmail.com>
2019-12-20 11:38:03 +0800

    no message

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -241,8 +241,9 @@ class MovieController extends Controller
         $review->movie_id = $movie_id;
         $review->animal_img_path = $img1;
         $review->food_img_path = $img2;
-        $review->save();
         dd($review);
+        $review->save();
+        
         return view('movies.confirm');
     }
 
